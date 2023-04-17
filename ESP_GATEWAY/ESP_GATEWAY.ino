51456e0b35210eb8a5f490d7560bc1fc62284151
Hyeqarl <130520840+Hyeqarl@users.noreply.github.com>
2023-04-17 08:06:03 +0800

    Update ESP_GATEWAY.ino

--- ESP_GATEWAY/ESP_GATEWAY.ino
+++ ESP_GATEWAY/ESP_GATEWAY.ino
@@ -8,11 +8,17 @@ static const char* ssid     = "Muhammad Haiqal's Vivo";
 static const char* password = "87654321";
 
 // ThingSpeak settings
-String serverName = "api.thingspeak.com";
-unsigned long channelID = 2108892;
-String apiKey = "9ZL2KOUDOAFFWKZ6";
-WiFiClient client;
-AsyncWebServer server(80);
+// Domain Name with full URL Path for HTTP POST Request
+const char* serverName = "http://api.thingspeak.com/update";
+// Service API Key
+String apiKey = "BL22P1HKXWQ8PMFA";
+//AsyncWebServer server(80);
+
+// THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
+// For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
+unsigned long lastTime = 0;
+unsigned long timerDelay = 100;
+
 
 // Structure to keep the temperature and humidity data
 // Is also required in the client to be able to save the data directly
@@ -64,22 +70,6 @@ void setup() {
   // Once the ESP-Now protocol is initialized, we will register the callback function
   // to be able to react when a package arrives in near to real time without pooling every loop.
   esp_now_register_recv_cb(OnRecv);
-
-server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
-    String url = "/update?" + apiKey + "&field1=" + String(analogRead(A0));
-    client.connect(serverName, 80);
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + serverName + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    delay(10);
-    while (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
-    client.stop();
-  });
-
-  server.begin();  
 }
 void loop() {
    // Wait for 10 seconds
@@ -89,26 +79,33 @@ void loop() {
   int value = random(100);
 
   // Send the data to ThingSpeak
-  if (client.connect(server.c_str(), 80)) {
-    String postData = apiKey;
-    postData += "&field1=";
-    postData += String(value);
-    postData += "\r\n\r\n";
-
-    client.print("POST /update HTTP/1.1\r\n");
-    client.print("Host: api.thingspeak.com\r\n");
-    client.print("Connection: close\r\n");
-    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\r\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\r\n");
-    client.print("Content-Length: ");
-    client.print(postData.length());
-    client.print("\r\n\r\n");
-    client.print(postData);
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+    
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverName);
+      
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // Data to send with HTTP POST
+      String temperatureData = "api_key=" + apiKey + "&field1=" + String(random(40));
+      String humidityData = "api_key=" + apiKey + "&field2=" + String(random(40));           
+      // Send HTTP POST request
+      int temperatureCode = http.POST(temperatureData);
+      int humidityCode = http.POST(humidityData);
+      Serial.print("HTTP Temperature Code: ");
+      Serial.println(temperatureCode);
+      Serial.print("HTTP Humidity Code: ");
+      Serial.println(humidityCode);
+        
+      // Free resources
+      http.end();
+    }
+    lastTime = millis();
   }
-  client.stop();
-
-  Serial.print("Value sent to ThingSpeak: ");
-  Serial.println(value);
   
 //IFTTT
 HTTPClient http;
@@ -117,7 +114,8 @@ HTTPClient http;
   int httpCode = http.GET();
   if (httpCode > 0) {
     Serial.printf("HTTP GET request sent, response code: %d\n", httpCode);
-  } else {
+  } 
+  else {
     Serial.println("HTTP GET request failed");
   }
   http.end();
